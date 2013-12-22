64274ed50247f497a19f414c0abe282119e8ab79
peterjoh <peter.joh@gmail.com>
2013-12-22 12:29:30 +0100

    Update Fraggel_03.py

--- Fraggel_03.py
+++ Fraggel_03.py
@@ -20,6 +20,9 @@ import random
 
 class Robot:
 
+    def __init__(self):
+        self.turn = -1
+
     def parse(self, game):
         # self.location
         # self.player_id
@@ -28,16 +31,11 @@ class Robot:
         self.locX   = self.location[0]
         self.locY   = self.location[1]
         self.game   = game
-        self.turn   = game['turn']
-        self.robots = game['robots']
 
-        # two dimensional list [x][y] filled with robots or None
-        self.matrix = [[None for x in xrange(19)] for y in xrange(19)]
-
-        for robot in self.robots:
-            x                 = robot[0]
-            y                 = robot[1]
-            self.matrix[x][y] = self.robots[robot]
+        if self.turn != game['turn']:
+            self.turn   = game['turn']
+            self.robots = game['robots']
+            self.scores = {} # location score cache
 
     def log(self, message):
         print "{0}: {1}".format(self.loc, message)
@@ -46,16 +44,33 @@ class Robot:
 
     # Is / can ######################################################
 
-    def can_move_to(self, loc):
+    def is_occupied(self, loc):
+        for l in self.robots:
+            if self.robots[l]['location'] == loc:
+                return True
+
+        return False
+
+    def is_loc_type_ok(self, loc):
         loc_types = rg.loc_types(loc)
+        bad_types = ['invalid', 'obstacle']
 
         for loc_type in loc_types:
-            # loc_type == 'spawn'   or \
-            if  loc_type == 'invalid' or \
-                loc_type == 'obstacle':
+            if loc_type in bad_types:
                 return False
 
-        return not self.is_occupied(loc)
+        return True
+
+    def has_ally_neighbor(self, loc):
+        allies = self.get_neighboring_allies(loc)
+        allies = [r for r in allies if r['location'] != self.location]
+
+        return len(allies) > 0
+
+    def can_move_to(self, loc):
+        return \
+            self.is_loc_type_ok(loc)  and \
+            not self.is_occupied(loc)
 
     def can_escape(self):
         outs    = self.get_available_neighbor_locations()
@@ -74,28 +89,16 @@ class Robot:
     def is_ally(self, robot):
         return robot['player_id'] == self.player_id
 
-    def is_alone(self):
-        return len(self.get_occupied_neighbors()) == 0
-
     def is_near_enemy(self):
         return len(self.get_neighboring_enemies()) > 0
 
     def is_outnumbered(self):
         allies  = self.get_neighboring_allies()
         enemies = self.get_neighboring_enemies()
+
+        # TODO: consider each enemys number of allies
         
         return len(enemies) > len(allies) + 1
-
-    def is_occupied(self, loc):
-        if self.loc == loc:
-            return True
-
-        neighbors = self.get_occupied_neighbors()
-        for r in neighbors:
-            if r['location'] == loc:
-                return True
-
-        return False
 
     def is_panic(self):
         enemies = len(self.get_neighboring_enemies())
@@ -108,6 +111,12 @@ class Robot:
 
     def is_someone_fighting(self):
         return len(self.get_fight_openings()) > 0
+
+    def is_in_spawn_area(self):
+        return 'spawn' in rg.loc_types(self.loc)
+
+    def is_empty(self, loc):
+        return loc not in self.robots
 
 
 
@@ -156,8 +165,6 @@ class Robot:
 
         locs.sort(key=lambda l: rg.dist(l, self.loc))
 
-        self.log("Nearest enemy openings: {0}".format(locs))
-
         return locs[0]
 
     def get_neighbor_locations(self, loc=None):
@@ -169,8 +176,8 @@ class Robot:
         return [l for l in ls if 0 <= l[0] < 19 and 0 <= l[1] < 19]
 
     def get_neighbors(self, loc=None):
-        m = self.matrix
-        return [m[x][y] for x, y in self.get_neighbor_locations(loc)]
+        rs = self.robots
+        return [rs[(x,y)] for x, y in self.get_neighbor_locations(loc) if (x,y) in rs]
 
     def get_available_neighbor_locations(self, loc=None):
         ls = self.get_neighbor_locations(loc)
@@ -207,14 +214,29 @@ class Robot:
  
 
 
-    # Low level actions #############################################
+    # RobotGame actions #############################################
 
     def guard(self):
         return ['guard']
 
+    def suicide(self):
+        return ['suicide']
+
+    def move(self, loc):
+        self.robots[loc] = self.robots[self.loc]
+        del self.robots[self.loc]
+        return ['move', loc]
+
+    def attack(self, loc):
+        return ['attack', loc]
+
+
+
+    # Low level actions #############################################
+
     def move_to(self, loc):
         if self.can_move_to(loc):
-            return ['move', loc]
+            return self.move(loc)
 
         alternatives = self.get_available_neighbor_locations()
         
@@ -223,22 +245,14 @@ class Robot:
             loc = alternatives[rnd]
 
             if self.can_move_to(loc):
-                return ['move', loc]
+                return self.move(loc)
 
         return self.guard()
-
-    def move_to_center(self):
-        loc = rg.toward(self.loc, rg.CENTER_POINT)
-
-        return self.move_to(loc)
 
     def move_toward(self, loc):
         loc = rg.toward(self.loc, loc)
 
         return self.move_to(loc)
-
-    def boom(self):
-        return ['suicide']
 
 
 
@@ -249,40 +263,38 @@ class Robot:
         loc   = enemy['location']
 
         self.log("Moving to nearest enemy at {0}".format(loc))
-
         return self.move_toward(loc)
-
-    def attack_enemy(self):
-        enemies = self.get_neighboring_enemies()
-        enemies.sort(key=lambda e: e['hp'])
-        loc = enemies[0]['location']
-
-        self.log("Fighting enemy at {0}".format(loc))
-
-        return ['attack', loc]
-
-    def flee(self):
-        outs = self.get_available_neighbor_locations()
-
-        outs.sort(key=lambda l: len(self.get_neighboring_enemies(l)))
-
-        self.log("Fleeing to {0}".format(outs[0]))
-
-        return ['move', outs[0]]
 
     def move_to_nearest_enemy_opening(self):
         loc = self.get_nearest_enemy_opening()
 
         self.log("Moving to nearest enemy opening at {0}".format(loc))
-
         return self.move_toward(loc)
 
     def move_to_nearest_fight_opening(self):
         loc = self.get_nearest_fight_opening()
 
         self.log("Moving to nearest fight opening at {0}".format(loc))
-
         return self.move_toward(loc)
+
+    def attack_weakest_enemy(self):
+        enemies = self.get_neighboring_enemies()
+        enemies.sort(key=lambda e: e['hp'])
+        loc = enemies[0]['location']
+
+        self.log("Fighting enemy at {0}".format(loc))
+        return self.attack(loc)
+
+    def flee(self):
+        outs = self.get_available_neighbor_locations()
+
+        if len(outs) == 0:
+            return self.guard()
+
+        outs.sort(key=lambda l: len(self.get_neighboring_enemies(l))*10 + random.randint(0, 9))
+
+        self.log("Fleeing to {0}".format(outs[0]))
+        return self.move(outs[0])
 
 
 
@@ -291,20 +303,23 @@ class Robot:
     def act(self, game):
         self.parse(game)
 
+        if self.is_in_spawn_area():
+            return self.flee()
+
         if self.is_near_enemy():
             if self.is_panic():
-                return self.boom()
+                return self.suicide()
             
             elif self.is_outnumbered() and self.can_escape():
                 return self.flee()
             
             else:
-                return self.attack_enemy()
+                return self.attack_weakest_enemy()
 
-        elif self.is_someone_fighting():
+        if self.is_someone_fighting():
             return self.move_to_nearest_fight_opening()
 
-        elif self.is_open_enemies():
+        if self.is_open_enemies():
             return self.move_to_nearest_enemy_opening()
 
         return self.guard()
